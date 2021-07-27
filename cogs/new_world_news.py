31d4d6bb6f8f047697da4bd98bf3e21c0599f71a
Andre Saddler <dresadd09691@gmail.com>
2021-07-27 18:47:56 -0400

    cogs: add New World v2 endpoint

--- cogs/new_world_news.py
+++ cogs/new_world_news.py
@@ -22,7 +22,7 @@ def getNWWUpdatesV1():
     return response.json()
 
 def getNWWUpdatesV2():
-    URL = "https://api.axsddlr.xyz/newworld/v2/updates"
+    URL = "https://api.axsddlr.xyz/newworld/v2/downtime"
     response = requests.get(URL)
     return response.json()
 
@@ -39,7 +39,7 @@ def updater(d, inval, outval):
 class NWW_Patch(commands.Cog, name="New World Patch Notes"):
     def __init__(self, bot):
         self.bot = bot
-        self.scheduler = AsyncIOScheduler(job_defaults={"misfire_grace_time": 900})
+        self.scheduler = AsyncIOScheduler(job_defaults={"misfire_grace_time": 800})
 
     async def nww_patch_monitor(self):
         await self.bot.wait_until_ready()
@@ -95,6 +95,61 @@ class NWW_Patch(commands.Cog, name="New World Patch Notes"):
 
         f.close()
 
+    async def nww_patch_monitorv2(self):
+        await self.bot.wait_until_ready()
+
+        saved_json = "nww_old_2.json"
+
+        # call API
+        responseJSON = getNWWUpdatesV2()
+
+        title = responseJSON["data"][0]["title"]
+        # description = responseJSON["data"][0]["description"]
+        thumbnail = responseJSON["data"][0]["thumbnail"]
+        url = responseJSON["data"][0]["url"]
+
+        # check if file exists
+        nww_exists(saved_json)
+
+        time.sleep(5)
+        # open saved_json and check title string
+        f = open(
+            saved_json,
+        )
+        data = json.load(f)
+        res = updater(data, "", None)
+        check_file_json = res["data"][0]["title"]
+
+        # compare title string from file to title string from api then overwrite file
+        if check_file_json == title:
+            # print("True")
+            return
+        elif check_file_json != title:
+            # print("False")
+            # hook = Webhook(nww_webhook)
+            # hook.send(full_url)
+
+            hook = Webhook(nww_webhook)
+
+            embed = Embed(
+                title="New World",
+                description=f"[{title}]({url})\n\n",
+                # description=f"[{title}]({url})\n\n{description}",
+                color=crimson,
+                timestamp="now",  # sets the timestamp to current time
+            )
+            embed.set_footer(text="Patch bot")
+            embed.set_image(url=thumbnail)
+            file = File("./assets/images/nw_logo.png", name="nw_logo.png")
+            embed.set_thumbnail(url="attachment://nw_logo.png")
+
+            hook.send(embed=embed, file=file)
+
+            f = open(saved_json, "w")
+            print(json.dumps(responseJSON), file=f)
+
+        f.close()
+
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -103,6 +158,7 @@ class NWW_Patch(commands.Cog, name="New World Patch Notes"):
 
         # add job for scheduler
         scheduler.add_job(self.nww_patch_monitor, "interval", seconds=1800)
+        scheduler.add_job(self.nww_patch_monitorv2, "interval", seconds=1805)
 
         # starting the scheduler
         scheduler.start()
--- utils/global_utils.py
+++ utils/global_utils.py
@@ -48,3 +48,26 @@ def matches_exists(s):
         # For other errors
         except:
             print("Error occurred while copying file.")
+
+def nww_exists(s):
+    path = pathlib.Path(s)
+    if path.exists():
+        return
+        # print("File exist")
+    else:
+        source = "./assets/empty_nww.json"
+        try:
+            shutil.copy(source, s)
+            print("File copied successfully.")
+
+        # If source and destination are same
+        except shutil.SameFileError:
+            print("Source and destination represents the same file.")
+
+        # If there is any permission issue
+        except PermissionError:
+            print("Permission denied.")
+
+        # For other errors
+        except:
+            print("Error occurred while copying file.")
