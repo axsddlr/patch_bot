83ca6e5726eb38f58372670002e27f2050a2d921
Andre Saddler <dresadd09691@gmail.com>
2021-08-01 03:15:30 -0400

    add valorant esports highlights

--- cogs/valorant.py
+++ cogs/valorant.py
@@ -24,6 +24,11 @@ def getVALREDUpdates():
     response = requests.get(URL)
     return response.json()
 
+def getVALCOMPREDUpdates():
+    URL = "https://api.axsddlr.xyz/reddit/ValorantComp"
+    response = requests.get(URL)
+    return response.json()
+
 
 def updater(d, inval, outval):
     for k, v in d.items():
@@ -101,12 +106,12 @@ class Valorant_Updates(commands.Cog, name="Valorant Updates"):
 
         f.close()
 
-    async def reddit_monitor(self):
+    async def valorant_monitor(self):
         await self.bot.wait_until_ready()
 
         # call API
         # patch-notes channel
-        saved_json = "reddit_old.json"
+        saved_json = "valorant_old.json"
         responseJSON = getVALREDUpdates()
 
         basetree = responseJSON["data"]["segments"][0]
@@ -155,6 +160,59 @@ class Valorant_Updates(commands.Cog, name="Valorant Updates"):
 
         f.close()
 
+    async def valorant_comp_monitor(self):
+        await self.bot.wait_until_ready()
+
+        # call API
+        # patch-notes channel
+        saved_json = "valorant_comp_old.json"
+        responseJSON = getVALCOMPREDUpdates()
+
+        basetree = responseJSON["data"]["segments"][0]
+
+        title = basetree["title"]
+        thumbnail = basetree["thumbnail_url"]
+        url_path = basetree["url_path"]
+        author = basetree["author"]
+        # description = basetree["selftext"]
+        flair = basetree["flair"]
+        full_url = "https://www.reddit.com" + url_path
+
+        # check if file exists
+        news_exists(saved_json)
+
+        time.sleep(5)
+        # open saved_json and check title string
+        f = open(
+            saved_json,
+        )
+        data = json.load(f)
+        res = updater(data, "", None)
+        check_file_json = res["data"]["segments"][0]["title"]
+
+        if (flair != "Highlight | Esports") and (check_file_json == title):
+            # print("not patch notes")
+            return
+        elif (flair == "Highlight | Esports") and (check_file_json != title):
+            # print("False")
+            hook = Webhook(reddit_webhook)
+
+            embed = Embed(
+                title="VALORANT ESPORTS HIGHLIGHTS",
+                description=f"[{title}]({full_url})\n\n author: {author}",
+                color=crimson,
+                timestamp="now",  # sets the timestamp to current time
+            )
+            embed.set_footer(text="Patch bot")
+            embed.set_image(url=thumbnail)
+            file = File("./assets/images/val_reddit.png", name="val_reddit.png")
+            embed.set_thumbnail(url="attachment://val_reddit.png")
+
+            hook.send(embed=embed, file=file)
+            f = open(saved_json, "w")
+            print(json.dumps(responseJSON), file=f)
+
+        f.close()
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -162,7 +220,8 @@ class Valorant_Updates(commands.Cog, name="Valorant Updates"):
 
         # add job for scheduler
         scheduler.add_job(self.valupdates, "interval", seconds=3600)
-        scheduler.add_job(self.reddit_monitor, "interval", seconds=1200)
+        scheduler.add_job(self.valorant_monitor, "interval", seconds=1200)
+        scheduler.add_job(self.valorant_comp_monitor, "interval", seconds=1235)
 
         # starting the scheduler
         scheduler.start()
